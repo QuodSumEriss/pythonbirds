8dd83c93bbdb12c1660a09161593c4a387636235
QuodSumEriss <joaopedro.silva.paulino@gmail.com>
2020-03-16 15:15:43 -0300

    Criado atributo complexo filhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('João')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Pedro'
-    print(p.nome)
-    print(p.idade)
+    pedro = Pessoa(nome='Pedro')
+    joão = Pessoa(pedro, nome='João')
+    print(Pessoa.cumprimentar(joão))
+    print(id(joão))
+    print(joão.cumprimentar())
+    print(joão.nome)
+    print(joão.idade)
+for filhos in joão.filhos:
+    print(filhos.nome)
